65c631697cc7797afa436e573fc3af977225b187
Kartik Chausali <kartikchausali@Kartiks-MacBook-Air.local>
2024-05-04 14:42:57 +0530

    type error

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -95,6 +95,6 @@ interface labelledInputsType {
 function LabelledInput({label, placeholder, onChange, type}: labelledInputsType){
     return <div>
     <label  className="block mb-2 text-sm font-semibold \ text-black pt-4">{label}</label>
-    <input onChange={onChange} type={type || ""} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required />
+    <input onChange={onChange} type={type || ""} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder}  />
 </div>
 }
